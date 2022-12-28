9572f0c60482146cd278827db505070e986d9dbc
Inna <inna.kornieko@gmail.com>
2022-12-28 21:11:07 +0200

    Done Avatar

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,4 +1,5 @@
 const {Conflict} = require("http-errors");
+const gravatar = require("gravatar");
 const {User} = require("../../models");
 
 
@@ -8,7 +9,9 @@ const signup = async(req, res) => {
     if(user) {
         throw new Conflict(`User with ${email} already exist`)
     }
-    const newUser = new User({email});
+
+    const avatarURL = gravatar.url(email);
+    const newUser = new User({email, avatarURL});
     newUser.setPassword(password);
     newUser.save();
    
@@ -19,7 +22,8 @@ const signup = async(req, res) => {
         data: {
           user: {
             email,
-            subscription: "starter"
+            subscription: "starter",
+            avatarURL,
           }
         }
     })
