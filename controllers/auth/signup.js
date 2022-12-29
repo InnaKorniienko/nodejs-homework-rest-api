620a2e2ef1e419fd25b707147fda59803402953e
Inna <inna.kornieko@gmail.com>
2022-12-29 21:32:00 +0200

    Add verification

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,5 +1,8 @@
 const {Conflict} = require("http-errors");
+const {nanoid} = require("nanoid");
 const gravatar = require("gravatar");
+const {sendEmail} = require("../../helpers");
+
 const {User} = require("../../models");
 
 
@@ -10,11 +13,19 @@ const signup = async(req, res) => {
         throw new Conflict(`User with ${email} already exist`)
     }
 
+    const verificationToken = nanoid();
     const avatarURL = gravatar.url(email);
-    const newUser = new User({email, avatarURL});
+    const newUser = new User({email, avatarURL, verificationToken});
     newUser.setPassword(password);
     newUser.save();
-   
+    
+    const mail = {
+      to: email,
+      subject: "Подтверждения email",
+      html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Подтвердить email</a>`
+  };
+  
+  await sendEmail(mail);
 
     res.status(201).json({
         status: "success",
@@ -24,6 +35,7 @@ const signup = async(req, res) => {
             email,
             subscription: "starter",
             avatarURL,
+            verificationToken
           }
         }
     })
